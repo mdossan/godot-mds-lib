f66eaa934c01ca27519b510ba6ff933374ebc8cd
mdossan <mdossan@protonmail.com>
2026-07-17 19:17:59 +0200

    ✨ Add sensitivity in `MdsCameraControl3D`

--- addons/mds_lib/skills/camera_control/camera_control.gd
+++ addons/mds_lib/skills/camera_control/camera_control.gd
@@ -1,6 +1,7 @@
 class_name MdsCameraControl
 extends Node3D
 
+@export var sensitivity: float = 0.5
 @export var node_vertical: Node3D
 @export var node_horizontal: Node3D
 @export var disabled: bool = false
@@ -15,12 +16,12 @@ func _input(event: InputEvent) -> void:
 		return
 	
 	if event is InputEventMouseMotion:
-		current_rot.x = current_rot.x - event.relative.x
+		current_rot.x = (current_rot.x - event.relative.x)
 		current_rot.y = clamp(current_rot.y - event.relative.y, -90, 35)
 
 func _process(delta: float) -> void:
 	if get_multiplayer_authority() != multiplayer.get_unique_id():
 		return
 	
-	node_vertical.rotation.x = deg_to_rad(current_rot.y)
-	node_horizontal.rotation.y = deg_to_rad(current_rot.x)
+	node_vertical.rotation.x = deg_to_rad(current_rot.y) * sensitivity
+	node_horizontal.rotation.y = deg_to_rad(current_rot.x) * sensitivity
