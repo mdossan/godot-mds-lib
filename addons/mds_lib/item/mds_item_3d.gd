9689772357fc718270c2aced71b4105a274129f1
mdossan <mdossan@protonmail.com>
2026-07-23 19:26:49 +0200

    ♻️ Update and organize `MdsItem3D`

--- addons/mds_lib/item/mds_item_3d.gd
+++ addons/mds_lib/item/mds_item_3d.gd
@@ -13,23 +13,31 @@ signal used(item: MdsItem3D, inventory: MdsInventoryBehavior3D)
 @export var item_resource: MdsItemResource:
 	set(new_value):
 		item_resource = new_value
-		if item_resource:
-			%Mesh.mesh = item_resource.mesh
+		#if item_resource:
+			#%Mesh.mesh = item_resource.mesh
 		update_configuration_warnings()
 @export var inventory: MdsInventoryBehavior3D: set = set_inventory
 
 var item_unique_id: int = ResourceUID.create_id()
 var item_timestamp: float = Time.get_unix_time_from_system()
 
+#region Godot's Lifecycle
+
+func _ready() -> void:
+	if Engine.is_editor_hint():
+		return
+	add_to_group(META)
+	if parent:
+		parent.set_meta(META, self)
+	set_meta(META, self)
+	#%Mesh.mesh = item_resource.mesh
+
+#endregion
+
 func set_inventory(new_inventory: MdsInventoryBehavior3D):
 	inventory = new_inventory
-	if get_multiplayer_authority() == multiplayer.get_unique_id():
+	if is_multiplayer_authority():
 		remote_set_inventory.rpc(new_inventory.get_path())
-
-@rpc("authority", "call_remote")
-func remote_set_inventory(inventory_path: NodePath):
-	var new_inventory = get_tree().root.get_node(inventory_path)
-	inventory = new_inventory
 
 func _get_configuration_warnings() -> PackedStringArray:
 	var warnings = []
@@ -37,17 +45,18 @@ func _get_configuration_warnings() -> PackedStringArray:
 		warnings.push_back("'Item Resource' should be defined")
 	return warnings
 
-func _ready() -> void:
-	if Engine.is_editor_hint():
-		return
-	
-	add_to_group(META)
-	
+func delete():
 	if parent:
-		parent.set_meta(META, self)
-	
-	set_meta(META, self)
-	%Mesh.mesh = item_resource.mesh
+		parent.queue_free()
+	else:
+		queue_free()
+
+#region RPCs
+
+@rpc("authority", "call_remote")
+func remote_set_inventory(inventory_path: NodePath):
+	var new_inventory = get_tree().root.get_node(inventory_path)
+	inventory = new_inventory
 
 @rpc("authority", "reliable")
 func remote_delete():
@@ -56,8 +65,4 @@ func remote_delete():
 	else:
 		queue_free()
 
-func delete():
-	if parent:
-		parent.queue_free()
-	else:
-		queue_free()
+#endregion
