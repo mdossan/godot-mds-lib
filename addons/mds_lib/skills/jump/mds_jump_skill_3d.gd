f3d9e2269004658bf8e464ead4b0fe6e753aa23f
mdossan <mdossan@protonmail.com>
2026-08-07 19:51:44 +0200

    ✨ Add signals to `MdsJumpSkill3D`

--- addons/mds_lib/skills/jump/mds_jump_skill_3d.gd
+++ addons/mds_lib/skills/jump/mds_jump_skill_3d.gd
@@ -1,5 +1,8 @@
 class_name MdsJumpSkill3D extends Node3D
 
+signal jump_started
+signal jump_ended
+
 @export var target_character_body_3d: CharacterBody3D
 
 @export_group("Jump Config")
@@ -8,11 +11,10 @@ class_name MdsJumpSkill3D extends Node3D
 @export var jump_speed: float = 1.8
 @export var jump_action: String = "jump"
 
-var is_pressing_jump: bool = false
 var jumping: bool = false
+var jump_in_progress: bool = false
 var jump_time: float = 0.0
 var jump_last_sample: float = 0.0
-var jump_pressed_amount: float = 0.0
 var jump_release_offset: float = 1.0
 var jump_release_time: float = 1.0
 
@@ -21,22 +23,26 @@ func _input(event: InputEvent) -> void:
 		return
 	
 	if event.is_action_pressed(jump_action):
-		is_pressing_jump = true
 		jumping = true
+		jump_started.emit()
 	
-	if event.is_action_released("jump"):
-		if jump_time < .5:
+	if event.is_action_released(jump_action):
+		if jumping and jump_time < .5:
 			jump_release_offset = jump_time
 			jump_release_time = 1 - jump_time
 
 func _physics_process(_delta: float) -> void:
-	if target_character_body_3d.is_on_floor():
+	if jump_in_progress and target_character_body_3d.is_on_floor():
+		jump_ended.emit()
+		jump_in_progress = false
 		jumping = false
-	
-	if not jumping:
 		jump_time = 0.0
 		jump_last_sample = 0.0
-		jump_pressed_amount = 0.0
+		jump_release_time = 1.0
+		jump_release_offset = 1.0
+		return
+	
+	if not jumping:
 		return
 	
 	jump_time += _delta * jump_speed
@@ -44,6 +50,9 @@ func _physics_process(_delta: float) -> void:
 		jump_release_offset = 1.0
 	var sample: float = jump_curve.sample(jump_time)
 	var jump_vector: float = (sample - jump_last_sample) * jump_release_offset
-	target_character_body_3d.velocity = target_character_body_3d.global_basis.y * jump_vector * jump_force
-	target_character_body_3d.move_and_slide()
+	var up_direction: Vector3 = target_character_body_3d.global_basis.y
+	target_character_body_3d.velocity += up_direction * jump_vector * jump_force
 	jump_last_sample = sample
+	jump_in_progress = true
+	if jump_time >= 1:
+		jump_ended.emit()
