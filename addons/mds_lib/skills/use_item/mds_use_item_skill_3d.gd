4197038910e738585a08218b51b557ec50bc2524
mdossan <mdossan@protonmail.com>
2026-08-10 14:02:33 +0200

    ✨ Add `item_used` signal in `MdsUseItemSkill3D`

--- addons/mds_lib/skills/use_item/mds_use_item_skill_3d.gd
+++ addons/mds_lib/skills/use_item/mds_use_item_skill_3d.gd
@@ -1,17 +1,12 @@
-class_name MdsUseItemSkill3D
-extends Node3D
+class_name MdsUseItemSkill3D extends Node3D
+
+signal item_used(item: MdsItem3D)
 
 @export var use_action: String = "use"
 @export var item_to_use: Node = null
 
 var item: MdsItem3D
 var _should_use: bool = false
-
-func _ready() -> void:
-	if not item_to_use.has_meta(MdsItem3D.META):
-		push_error("item should have metadata")
-		return
-	item = item_to_use.get_meta(MdsItem3D.META)
 
 func _input(event: InputEvent) -> void:
 	if multiplayer.get_unique_id() != get_multiplayer_authority():
@@ -21,15 +16,20 @@ func _input(event: InputEvent) -> void:
 		_should_use = true
 
 func _physics_process(_delta: float) -> void:
+	if not is_instance_valid(item_to_use):
+		return
+	
 	if !_should_use:
 		return
 	
 	# Item usage is a "one time" input
 	_should_use = false
 	
+	item = item_to_use.get_meta(MdsItem3D.META)
 	if !is_instance_valid(item):
+		push_error("Item is invalid", item)
 		return
 	
 	var usage: MdsItemUsage = item.item_resource.usage
 	usage.use(item)
-	
+	item_used.emit(item)
