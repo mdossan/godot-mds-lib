b8806bcb32d985b1896e5ee6869c785ae611ce17
mdossan <mdossan@protonmail.com>
2026-08-24 16:57:37 +0200

    🔥 Remove unused `MdsVelocityResetBehavior3D`

--- addons/mds_lib/behaviors/mds_velocity_reset_behavior_3d/mds_velocity_reset_behavior_3d.gd
+++
@@ -1,6 +0,0 @@
-class_name VelocityResetBehavior3D extends Node3D
-
-@export var character_body_3d: CharacterBody3D
-
-func _physics_process(delta: float) -> void:
-	character_body_3d.velocity = Vector3.ZERO
