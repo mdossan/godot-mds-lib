f9b3ff6545932d6d070871fbac24f47cd0c19cae
mdossan <mdossan@protonmail.com>
2026-08-24 16:57:23 +0200

    🔥 Remove unused `ApplyVelocityBehavior3D`

--- addons/mds_lib/behaviors/apply_velocity_behavior_3d/mds_apply_velocity_behavior_3d.gd
+++
@@ -1,6 +0,0 @@
-class_name MdsApplyVelocityBehavior3D extends Node3D
-
-@export var character_body_3d: CharacterBody3D
-
-func _physics_process(_delta: float) -> void:
-	character_body_3d.move_and_slide()
